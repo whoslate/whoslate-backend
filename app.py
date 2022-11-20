9dd7a735d9c5c1bb91a2a4640813fe20981d4305
Linbin <i@teenet.me>
2022-11-19 21:43:57 -0500

    Apply JWT

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ Flask entry
 import os
 from flask import Flask
 from dotenv import load_dotenv
+from flask_jwt_extended import JWTManager
+
 from src.model import db, migrate
 from src import api_blueprint
 
@@ -18,8 +20,10 @@ def create_app() -> Flask:
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DB_URL']
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['JWT_SECRET_KEY'] = os.environ['JWT_SECRET_KEY']
     db.init_app(app)
     migrate.init_app(app, db)
+    JWTManager(app)
     app.register_blueprint(api_blueprint, url_prefix='/api')
 
     return app
